a8efb2765bdbfabcf7e0a093b66739457a2eae05
Forum Ganatra <forum.tanna@external.milliporesigma.com>
2024-09-20 17:38:50 +0530

    delete and edit functionality

--- apps/web/__tests__/page.test.tsx
+++ apps/web/__tests__/page.test.tsx
@@ -1,6 +1,6 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
-import Home from "../page";
+import Home from "../app/page";
 
 describe("Page", () => {
   it("renders a heading", () => {
