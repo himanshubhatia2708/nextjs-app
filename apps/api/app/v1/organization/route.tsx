e0694742ac56d856db6f788cc97c7780d0797cd5
Forum Ganatra <forum.tanna@external.milliporesigma.com>
2024-09-25 20:48:18 +0530

    merge code

--- apps/api/app/v1/organization/route.tsx
+++ apps/api/app/v1/organization/route.tsx
@@ -26,15 +26,6 @@ export async function POST(request: Request) {
   const req = await request.json();
   const { name, firstName, lastName, email } = req;
 
-  // Validate the incoming data
-  if (!name || !email) {
-    const message = "Missing required fields.";
-    return new Response(JSON.stringify(message), {
-      headers: { "Content-Type": "application/json" },
-      status: 500,
-    });
-  }
-
   // Check if an organization with the same name already exists (case insensitive)
   try {
     const existingOrganization = await prisma.organization.findFirst({
@@ -101,76 +92,4 @@ export async function POST(request: Request) {
   } catch (error) {
     console.error(error);
   }
-}
-
-export async function PUT(request: Request) {
-  try {
-    const req = await request.json();
-    const { id, name, firstName, lastName, email } = req;
-
-    // Validate the incoming data
-    if (!id || !name || !email) {
-      const message = "Missing required fields.";
-      return new Response(JSON.stringify(message), {
-        headers: { "Content-Type": "application/json" },
-        status: 500,
-      });
-    }
-
-    // Check if the organization exists
-    const existingOrganization = await prisma.organization.findUnique({
-      where: { id },
-      include: {
-        user: true,
-      },
-    });
-
-    if (!existingOrganization) {
-      const message = `Organization with ID "${id}" not found.`;
-      return new Response(JSON.stringify(message), {
-        headers: { "Content-Type": "application/json" },
-        status: 500,
-      });
-    }
-
-    // Update the organization and user details
-    const updatedOrganization = await prisma.organization.update({
-      where: { id },
-      data: {
-        name,
-        user: {
-          update: {
-            firstName,
-            lastName,
-            email,
-          },
-        },
-      },
-      include: {
-        user: {
-          include: {
-            user_role: {
-              include: {
-                role: true, // Include role details if needed
-              },
-            },
-          },
-        },
-      },
-    });
-
-    return new Response(JSON.stringify(updatedOrganization), {
-      headers: { "Content-Type": "application/json" },
-      status: 200,
-    });
-  } catch (error: any) {
-    console.error(error);
-    return new Response(JSON.stringify({ error: error.message }), {
-      headers: { "Content-Type": "application/json" },
-      status: 400, // Adjust status code as needed
-    });
-  } finally {
-    await prisma.$disconnect();
-  }
-
 }
